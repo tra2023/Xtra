466393133c91b08080d552a8bb16e5595c80a689
max <max@mustermann.de>
2026-09-09 17:51:46 +0200

    use subList instead of repeat

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/chat/ChatViewModel.kt
@@ -1001,9 +1001,7 @@ class ChatViewModel(
                 Triple(message, chatMessages.lastIndex, removeCount)
             } else {
                 if (removeCount > 0) {
-                    repeat(removeCount) {
-                        chatMessages.removeAt(0)
-                    }
+                    chatMessages.subList(0, removeCount).clear()
                 }
                 null
             }
